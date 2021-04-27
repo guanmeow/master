bb46e8265fd92dfd3f92eac37c7ca0cce3d1168a
GM <nicoyazawa0501@gmail.com>
2021-04-27 15:25:10 +0800

    0427 box enter number

--- src/main/java/gm/chen/Taster.java
+++ src/main/java/gm/chen/Taster.java
@@ -1,13 +1,19 @@
 package gm.chen;
 
+import java.util.Scanner;
+
 public class Taster {
     public static void main(String[] args) {
 //        Box1 box1 = new Box1();
+        Scanner scan = new Scanner(System.in);
 
         Box[] boxes = {new Box1(),new Box2(),new Box3(),new Box4()};
-        int leight=12;
-        int width=5;
-        int height=13;
+        System.out.println("enter leight");
+        int leight=scan.nextInt();
+        System.out.println("enter width");
+        int width=scan.nextInt();
+        System.out.println("enter height");
+        int height=scan.nextInt();
         for(Box box: boxes){
             if(box.volidate(leight,width,height)){
                 System.out.println(box.name +" it is. $ "+ box.price);
